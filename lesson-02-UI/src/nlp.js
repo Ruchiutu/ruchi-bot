e9e807aea60a491c9c661c03ac4dce4fa5eaa512
Ruchiutu <Ruchi@utu.ai>
2017-04-19 11:10:04 +0530

    second commit

---
+++ lesson-02-UI/src/nlp.js
@@ -0,0 +1,26 @@
+export const BOARDS = {
+  intents: {
+    TOP_BOARDS: {
+    },
+  },
+};
+
+export const CLIPS = {
+  intents: {
+    LIST_CATEGORIES: {
+    },
+    CLIP_CATEGORIES: {
+    },
+  },
+}
+
+export const SYSTEM = {
+  intents: {
+    START: {
+    },
+    HELP: {
+    },
+    STOP: {
+    },
+  },
+}
